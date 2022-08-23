0a1dd79cf0eb943465b0a63cad103fdfe33c5a79
Aaron Fowlkes <afowlkes21@gmail.com>
2022-08-23 14:51:42 -0400

    working on player turn

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,8 @@
 class Player < ApplicationRecord
 
+    $put_points = 5
+    $move_points = 5
+
     $board = {
         '11': nil, '21': nil, '31': nil, '41': nil,
         '51': nil, '61': nil, '71': nil, '81': nil,
@@ -20,7 +23,8 @@ class Player < ApplicationRecord
     }
 
     def put_down(level, h_pos, v_pos)
-        if @put_points <= 0
+        $put_points = $put_points - level.to_i
+        if $put_points < 0
             puts "You are out of put points"
             return
         end
@@ -34,43 +38,54 @@ class Player < ApplicationRecord
             puts "Cannot place there"
             return
         end
-        position = h_pos.to_str + v_pos.to_str
-        if board.position
+        position = h_pos.to_s + v_pos.to_s
+        position = position.to_sym
+        if $board[position]
             puts "Cannot place there"
             return
         else
             if username == "Player 1"
                 piece = Piece.create!(level: level, h_pos: h_pos, v_pos: v_pos, player: 1)
-                pos = h_pos.to_str + v_pos.to_str
+                pos = h_pos.to_s + v_pos.to_s
                 pos = pos.to_sym
                 $board[pos] = piece
             end
                 if username == "Player 2"
                 piece = Piece.create!(level: level, h_pos: h_pos, v_pos: v_pos, player: 2)
-                pos = h_pos.to_str + v_pos.to_str
+                pos = h_pos.to_s + v_pos.to_s
                 pos = pos.to_sym
                 $board[pos] = piece
             end
-            @put_points = @put_points - level
+            
         end
     end
 
-    def move_piece(new_position)
-        t = self.position
+    def move_piece(piece, new_position)
+        puts piece
+        $move_points = $move_points - 1
+        if $move_points < 0
+            puts "You cannot move another piece."
+            return
+        end
+        t_h_pos = piece.h_pos.to_s
+        t_r_pos = piece.r_pos.to_s
+        t = t_h_pos + t_r_pos
+        t = t.to_sym
         new_h_pos = ((new_position * 1.0) / 10).to_int
         new_v_pos = new_position % 10
         if Math.sqrt((h_pos - new_h_pos)**2 + (v_pos - new_v_pos)**2) > 1.5
             puts "One tile at a time"
             return
         end
-        if board.new_pos
-            self.attack(board.new_pos)
+        new_pos = new_h_pos.to_s + new_v_pos.to_s
+        new_pos = new_pos.to_sym
+        if Player.board[new_pos]
+            piece.attack(Player.board[new_pos])
         else
-            self.h_pos = new_h_pos
-            self.v_pos = new_v_pos
+            piece[h_pos] = new_h_pos
+            piece[v_pos] = new_v_pos
         end
-        board.t = nil
-
+        Player.board[t] = nil
     end
 
     def attack(attacked_piece)
@@ -85,29 +100,59 @@ class Player < ApplicationRecord
     end
 
     def self.board
-        puts $board
+        # puts $board
+        return $board
+        # 8.times do |r|
+        #     8.times do |c|
+        #         row = (r+1).to_s
+        #         col = (c+1).to_s
+        #         coord = row + col
+        #         coord = coord.to_sym
+        #         puts coord
+        #         if $boord[coord]==false
+        #             print("empty ")
+        #         else
+        #             print $boord[coord]
+        #         # puts $board[coord]
+        #         end
+        #     end
+        #     puts
+        # end
+            
         # x = "11"
         # x = x.to_sym
         # puts $board[x]
     end
 
     def turn
-        @put_points = 5
-        move_points = 5
-        puts "What action would you like to take? Say 'put piece' or 'move piece' or 'end turn'"
-        action = gets.chomp
-        puts "Action: " + action
-        if action =="put piece"
-            puts "What level?"
-            level = gets.chomp
-            puts "Horizontal position?"
-            h_pos = gets.chomp
-            h_pos = h_pos.to_i
-            puts "Vertical position?"
-            v_pos = gets.chomp
-            v_pos = v_pos.to_i
-            ##
-            self.put_down(level, h_pos, v_pos)
+       
+        while ($put_points > 0 || @move_points > 0)
+            puts "What action would you like to take? Say 'put piece' or 'move piece' or 'end turn'"
+            action = gets.chomp
+            puts "Action: " + action
+            if action == "put piece"
+                puts "What level?"
+                level = gets.chomp
+                puts "Horizontal position?"
+                h_pos = gets.chomp
+                h_pos = h_pos.to_i
+                puts "Vertical position?"
+                v_pos = gets.chomp
+                v_pos = v_pos.to_i
+                ##
+                self.put_down(level, h_pos, v_pos)
+            end
+            if action == "move piece"
+                puts "From where? (enter a coordinate in number form, such as 12)"
+                origin = gets.chomp
+                origin.to_sym
+                piece = Player.board[origin]
+                puts "To where? (enter a coordinate in number form, such as 12)"
+                destination = gets.chomp
+                destination.to_i
+                ##
+                self.move_piece(piece, destination)
+            end
         end
 
 
